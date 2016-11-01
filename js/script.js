d7f6fdd8f74b651d959ce6ae259c279327c56286
xiayuyingyun <978752808@qq.com>
2016-11-01 12:06:28 +0800

    update

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+
+jQuery(document).ready(function ($) {
+
+    /* Chart numbers absolute centering */
+
+    var chart = $('.chart'),
+        chartNr = $('.chart-content'),
+        chartParent = chart.parent();
+
+    function centerChartsNr() {
+
+        chartNr.css({
+            top: (chart.height() - chartNr.outerHeight()) / 2
+        });
+
+    }
+
+    if (chart.length) {
+
+        centerChartsNr();
+        $(window).resize(centerChartsNr);
+
+        chartParent.each(function () {
+
+            $(this).onScreen({
+                doIn: function () {
+                    $(this).find('.chart').easyPieChart({
+                        scaleColor: false,
+                        lineWidth: 12,
+                        size: 178,
+                        trackColor: false,
+                        lineCap: 'square',
+                        animate: 2000,
+                        onStep: function (from, to, percent) {
+                            $(this.el).find('.percent').text(Math.round(percent));
+                        }
+                    });
+                },
+            });
+
+            $(this).find('.chart').wrapAll('<div class="centertxt" />');
+
+        });
+
+    }
+
+    /* Side mockups fixes */
+
+    
+});
